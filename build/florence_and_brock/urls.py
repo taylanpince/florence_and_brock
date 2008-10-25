e2f116100686aa7176ee9b619ec01360622b0bf5
Sam Bull <sbull@trapeze.com>
2008-10-25 18:24:35 -0400

    Merged conflict in urls.py.

--- build/florence_and_brock/urls.py
+++ build/florence_and_brock/urls.py
@@ -5,10 +5,18 @@ from django.conf.urls.defaults import *
 admin.autodiscover()
 
 
+home_dict = {
+    'cat_slug': 'featured',
+    'num_per_page': 6,
+}
+
 urlpatterns = patterns('',
+    (r'^$', 'categorized_blog.views.categorized_posts', home_dict),
+    (r'^blog/', include('blog.urls')),
+
     # Admin
     (r'^admin/(.*)', admin.site.root),
-    
+
     # Temporary Base Template
     (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'base.html'}),
 )
