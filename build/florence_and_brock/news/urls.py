b2f95fe455f4f9629a58b26288557e3cc8bd9526
Djoume Salvetti <dsalvetti@trapeze.com>
2008-11-15 16:15:22 -0500

    Added Archive pages

--- build/florence_and_brock/news/urls.py
+++ build/florence_and_brock/news/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('news.views',
         url(r'^$', 'newsitem_list', name="newsitem_list"),
+        url(r'^archive/$', 'newsitem_archive', name="newsitem_archive"),
         url('^(?P<year>\d{4})/(?P<month>\d{2})/(?P<slug>[\w-]+)/$', 'detail',
             name="newsitem_detail"),
         )
